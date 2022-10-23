cf2a9b29a7a308ba30543c5bda8e5f910b729c11
LiR4 <pedrolira521@gmail.com>
2022-10-23 18:51:32 -0300

    att error db ok

--- SESI_Backend/config/dbConfig.js
+++ SESI_Backend/config/dbConfig.js
@@ -6,7 +6,7 @@ const Sequelize = require('sequelize'); // Vai receber o squelize
 // Conectar com banco de dados
 const connection = new Sequelize('SESI','api','passwd',{
     dialect: 'mysql',
-    host:'10.105.75.85',
+    host:'192.168.15.8',
     port: 30306,
 });
 
